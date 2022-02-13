5b56fd2d2c9102f6dc6f26e0b25327ed68d32a8e
lmj101910 <71292878+lmj101910@users.noreply.github.com>
2022-02-13 15:28:41 +0900

    Create 문자열 내림차순으로 배치하기.py

---
+++ Level 1/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = ''
+    str_list = list(map(str, s))
+    str_upper = []
+    str_lower = []
+    
+    for i in str_list:
+        if i.upper() == i:
+            str_upper.append(i)
+        elif i.lower() == i:
+            str_lower.append(i)
+            
+    str_upper.sort(reverse=True)
+    str_lower.sort(reverse=True)
+    
+    answer += ''.join(str_lower)
+    answer += ''.join(str_upper)
+
+    return answer
